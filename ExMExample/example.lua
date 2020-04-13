78c4ce842f4998647df2a63098f88cb962326734
Smallo <smallo92@gmail.com>
2020-04-14 09:45:18 +1000

    Added Comments

--- ExMExample/example.lua
+++ ExMExample/example.lua
@@ -1,3 +1,6 @@
+-- As you can see because of importing the shared file in the manifest we don't have to store the ESX table up the top
+-- nor do we have to have a sharedobject loop trying to be populated
+
 RegisterCommand("create", function(source, args)
 	local coords = GetEntityCoords(PlayerPedId())
 	-- This function below is an old ESX one but because we are importing using ExM instead, we use the ExM namespace instead
--- ExMExample/fxmanifest.lua
+++ ExMExample/fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'bodacious'
 game 'gta5'
 
+-- All we need to do is simply supply this shared script, currently it imports all the ESX/ExM functions.
+-- In the future it is also planned to import all the locales as well, so we won't even need to inlcude those
+-- in our manifests.
 shared_script "@extendedmode/imports.lua"
 
 client_script 'example.lua'
